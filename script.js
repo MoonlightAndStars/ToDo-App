f8a4e0527b574843fc02a22807fa2971ce6aedce
Sajjad <156933056+MoonlightAndStars@users.noreply.github.com>
2024-05-06 12:48:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("Empty task cannot be added to the list");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveTask();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveTask();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveTask();
+    }
+  },
+  false
+);
+
+
+function saveTask() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
